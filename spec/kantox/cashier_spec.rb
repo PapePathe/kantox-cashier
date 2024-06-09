4176edfe9afeac9c898f369b9aad6072db051950
Pathe <pathe.sene@gmail.com>
2024-06-09 20:26:31 +0000

    raise error if inputs are not numbers

--- spec/kantox/cashier_spec.rb
+++ spec/kantox/cashier_spec.rb
@@ -4,4 +4,27 @@ RSpec.describe Kantox::Cashier do
   it "has a version number" do
     expect(Kantox::Cashier::VERSION).not_to be nil
   end
+
+  it "raise error when items count or unit price is not a number" do
+    expect { Kantox::Cashier::DefaultRule.call("44", 100) }.to(
+      raise_error(Kantox::Cashier::InputIsNotANumber)
+    )
+    expect { Kantox::Cashier::DefaultRule.call(44, "100") }.to(
+      raise_error(Kantox::Cashier::InputIsNotANumber)
+    )
+
+    expect { Kantox::Cashier::PricePercentDiscountRule.call("44", 100) }.to(
+      raise_error(Kantox::Cashier::InputIsNotANumber)
+    )
+    expect { Kantox::Cashier::PricePercentDiscountRule.call(44, "100") }.to(
+      raise_error(Kantox::Cashier::InputIsNotANumber)
+    )
+
+    expect { Kantox::Cashier::PriceDiscountRule.call("44", 100) }.to(
+      raise_error(Kantox::Cashier::InputIsNotANumber)
+    )
+    expect { Kantox::Cashier::PriceDiscountRule.call(44, "100") }.to(
+      raise_error(Kantox::Cashier::InputIsNotANumber)
+    )
+  end
 end
